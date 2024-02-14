a841b073db380971567f9b0cbd29905537b46a23
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-14 18:19:11 -0500

    [Update] - Import and export classes.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-import { nombreCliente, ahorro, mostrarInfo, tieneSaldo } from "./cliente.js";
+import { nombreCliente, ahorro, mostrarInfo, tieneSaldo, Cliente } from "./cliente.js";
 console.log(nombreCliente);
 console.log(ahorro);
 
 console.log(mostrarInfo(nombreCliente, ahorro));
 console.log(tieneSaldo(ahorro));
+
+const cliente = new Cliente(nombreCliente, ahorro);
+console.log(cliente.mostrarInformacion());
--- js/cliente.js
+++ js/cliente.js
@@ -6,8 +6,8 @@ export const ahorro = 200;
 //   console.log('Desde un IIFE.');
 // })();
 
-export function mostrarInfo(nombreCliente, ahorro) {
-  return `El cliente ${nombreCliente} y tiene un ahorro de ${ahorro}.`;
+export function mostrarInfo(nombre, ahorro) {
+  return `El cliente ${nombre} y tiene un ahorro de ${ahorro}.`;
 }
 
 export function tieneSaldo(ahorro) {
@@ -17,3 +17,13 @@ export function tieneSaldo(ahorro) {
     console.log('Sin fondos.');
   }
 }
+
+export class Cliente {
+  constructor(nombre, ahorro){
+    this.nombre = nombre;
+    this.ahorro = ahorro;
+  }
+  mostrarInformacion(){
+    return `El cliente ${this.nombre} y tiene un ahorro de ${this.ahorro}.`;
+  };
+};
