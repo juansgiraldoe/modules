f6b4c7e477c5110384dcd3e472e95b3b21ef2a0a
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-14 18:11:32 -0500

    [Update] - Export functions.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
-import { nombreCliente, ahorro } from "./cliente.js";
+import { nombreCliente, ahorro, mostrarInfo, tieneSaldo } from "./cliente.js";
 console.log(nombreCliente);
 console.log(ahorro);
+
+console.log(mostrarInfo(nombreCliente, ahorro));
+console.log(tieneSaldo(ahorro));
--- js/cliente.js
+++ js/cliente.js
@@ -5,3 +5,15 @@ export const ahorro = 200;
 // (function () {
 //   console.log('Desde un IIFE.');
 // })();
+
+export function mostrarInfo(nombreCliente, ahorro) {
+  return `El cliente ${nombreCliente} y tiene un ahorro de ${ahorro}.`;
+}
+
+export function tieneSaldo(ahorro) {
+  if (ahorro>0) {
+    console.log('Si tiene saldo');
+  }else{
+    console.log('Sin fondos.');
+  }
+}
